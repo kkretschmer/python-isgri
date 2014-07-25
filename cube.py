dfb78e84e3f117cbaf637701adf782e3c0e08a22
Karsten Kretschmer <kkretsch@apc.univ-paris7.fr>
2014-07-25 17:49:53 +0200

    cube.py: summary fn spectrum and corr_shad, show

--- cube.py
+++ cube.py
@@ -4,6 +4,7 @@ access INTEGRAL/ISGRI IDL data cubes
 
 import astropy.io.fits
 import numpy as np
+import matplotlib.pyplot as plt
 
 class Cube:
     bin_width = 0.5 * (1 + np.round(0.054 * np.arange(0, 257)))
@@ -31,3 +32,40 @@ class Cube:
             self.low_threshold = np.zeros((128, 128, 256), np.float32)
             self.valid = np.zeros((128, 128), np.float32)
 
+    def spectrum(self):
+        return self.counts.sum((1, 2))
+
+    def corr_shad(self):
+        rate = np.array(self.counts, dtype='float64')
+        rate /= self.duration * 0.4**2
+        mdu_origins = (
+            (96, 64), (64, 64), (32, 64), (0, 64),
+            (96, 0), (64, 0), (32, 0), (0, 0))
+        for i in range(0, 8):
+            origin = mdu_origins[i]
+            rate[:, origin[0]:origin[0]+32, origin[0]:origin[0]+64] /= \
+                self.mdu_eff[i]
+        return(rate)
+
+    def show(self, e_min=25.0, e_max = 80.0, *args, **kwargs):
+        e = np.logical_and(self.e_min >= e_min, self.e_max <= e_max)
+        print(self.e_min[e])
+        shd = self.corr_shad()[e, :, :].sum(0)
+        dpi = np.zeros((134, 130))
+        for y in (64, 65):
+            dpi[:, y] = np.nan
+        for z in (32, 33, 66, 67, 100, 101):
+            dpi[z, :] = np.nan
+        for y in range(0, 2):
+            for z in range(0, 4):
+                ys0, ys1 = y * 64, y * 64 + 64
+                zs0, zs1 = z * 32, z * 32 + 32
+                yd0, yd1 = y * 66, y * 66 + 64
+                zd0, zd1 = z * 34, z * 34 + 32
+                dpi[zd0:zd1, yd0:yd1] = shd[zs0:zs1, ys0:ys1]
+        plt.imshow(dpi, aspect='equal', interpolation='nearest',
+                   origin='lower', extent=(-0.5, 129.5, -0.5, 133.5),
+                   *args, **kwargs)
+        # plt.plot([0, 1])
+        plt.show()
+        plt.close()
